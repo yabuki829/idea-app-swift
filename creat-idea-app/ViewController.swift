b9572d4f8c32eb6e2d5fd9059f89fcd144f50f85
yabuki829 <syodai829@icloud.com>
2023-04-09 15:03:56 +0900

    Initial Commit

---
+++ creat-idea-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  creat-idea-app
+//
+//  Created by 薮木翔大 on 2023/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

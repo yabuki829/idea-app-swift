65f0b86368625357d1c7696e32614593c8209499
yabuki829 <syodai829@icloud.com>
2023-04-10 18:02:12 +0900

    文字が上から下に永遠に落ち続ける機能

--- creat-idea-app/ViewController/WordViewController.swift
+++ creat-idea-app/ViewController/WordViewController.swift
@@ -1,53 +1,43 @@
 import UIKit
 
 class WordViewController: UIViewController {
-
-//    let word = ["Word1", "Word2", "Word3", "Word4", "Word5"] // 流す単語の配列
-    var timers: [Timer] = [] // アニメーションを制御するタイマーの配列
-    var labels: [UILabel] = [] // UILabelの配列
-
+    var labels = [UILabel]()
     override func viewDidLoad() {
         super.viewDidLoad()
+      
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // ラベルの位置と単語を初期設定
-        for i in 0..<word.count {
+        for i in 0..<word.count{
             let label = UILabel()
             view.addSubview(label)
+            label.text = word[i]
             label.textColor = .white
             label.textAlignment = .left
-            label.frame = CGRect(x: CGFloat.random(in: -50..<view.bounds.width-20), y: -50, width: view.frame.width , height: 20) // ランダムな位置に配置
-            label.text = word[i]
-            labels.append(label)
-        }
-
-        startAnimation()
-    }
-
-    func startAnimation() {
-        for i in 0..<labels.count {
-            let label = labels[i]
-            let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval.random(in: 0.1...10.0), repeats: true) { [weak self] (_) in
-                self?.animateLabel(label) // ランダムなタイミングでアニメーションを開始
+            label.frame = CGRect(x: CGFloat.random(in: -50..<(view.bounds.width-20)), y: -50, width: view.frame.width, height: 20)
+//            labels.append(label)
+            // アニメーション実行中の配列
+            Timer.scheduledTimer(withTimeInterval: TimeInterval.random(in: 5...100.0), repeats: false) { (_) in
+                // 1秒後に実行されるコード
+                self.startAnimationLabel(label)
             }
-//            timers.append(timer)
         }
+        
+        
     }
 
-    func animateLabel(_ label: UILabel) {
 
-        // CABasicAnimationを使ってラベルをアニメーションさせる
-        let animation = CABasicAnimation(keyPath: "position")
-        animation.fromValue = NSValue(cgPoint: label.center)
-        animation.toValue = NSValue(cgPoint: CGPoint(x: label.center.x, y:view.frame.height))
-        animation.duration = 5.0
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        animation.isRemovedOnCompletion = false
-
-        label.layer.add(animation, forKey: "positionAnimation") // アニメーションを追加
+    func startAnimationLabel(_ label: UILabel) {
+        UIView.animate(withDuration: 8.0, delay: 0.0, options: .curveEaseInOut, animations: {
+            label.frame.origin.y = self.view.frame.height + 100
+        }, completion: { (_) in
+            label.frame.origin.y = -50
+            self.startAnimationLabel(label)
+        })
     }
+
+   
 
     
 }
